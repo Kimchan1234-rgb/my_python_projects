a9104f72cd00bbe0368010ae417651c3f2c65c4d
Denis <Denisangrygamer@gmail.com>
2026-06-18 14:47:18 +0300

    первый Python файл

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Привет, Денис!")
+print("Мой первый Python код!")
+
+vozrast = 34
+imya = "Denis"
+rost = 182.2
+
+print("Меня зовут " + imya)
+print("Мой возраст: " + str(vozrast))
+print("Мой рост: " + str(rost))
