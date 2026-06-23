106a19c1929d99208091343648009848dd8ebd3a
Denis <Denisangrygamer@gmail.com>
2026-06-23 21:53:35 +0300

    день 18 - математика

---
+++ day17.py
@@ -0,0 +1,24 @@
+imya = "Denis Oleksandrovich"
+
+print(imya.upper())
+print(imya.lower())
+print(imya.replace("Denis", "Денис"))
+
+print(len(imya))
+
+print(imya[0])
+print(imya[0:5])
+print(imya[-1])
+
+slova = imya.split(" ")
+print(slova)
+
+print(imya.startswith("Denis"))
+print(imya.endswith("ich"))
+
+gorod = "poltava city"
+print(f"Я живу в {gorod.title()}")
+
+
+
+
---
+++ day18.py
@@ -0,0 +1,23 @@
+
+a = 10
+b = 3
+
+print(a + b)   
+print(a - b)    
+print(a * b) 
+print(a / b)   
+print(a // b)  
+print(a % b)    
+print(a ** b)   
+
+import math
+print(math.sqrt(144))
+print(math.ceil(3.2))
+print(math.floor(3.9))
+print(round(3.567, 2))
+
+ves = 73.4
+rost_m = 1.822
+
+imt = ves / (rost_m ** 2)
+print(f"Твой ИМТ: {round(imt, 1)}")
